e670bc53d645da56fa3ea5fae20419d4da493268
eliud kagema <eliud-kagema@users.noreply.github.com>
2021-05-09 10:56:29 +0300

    setting up the inventory models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Retailer(models.Model):
+    ...
+
+class Outlet(models.Model):
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    county = models.CharField(max_length=200)
+    constituency = models.CharField(max_length=200)
+    ward = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    gps_coordinates = models.CharField(max_length=200)
+    outlet_code = models.CharField(max_length=200)
+    outlet_person = models.CharField(max_length=200)
+    number = models.CharField(max_length=200)
+    amenities = models.CharField(max_length=200)
+
+
+
+class Product(models.Model):
+    ...
+
+
+class ProductCategory(models.Model):
+    ...
+
+class Products(models.Model):
+    ...
