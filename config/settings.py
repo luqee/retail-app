7c20f6067bd9dc89ac73395c60991341e5849344
eliud kagema <eliud-kagema@users.noreply.github.com>
2021-05-09 12:58:14 +0300

    setting up retailer appp

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventory.apps.InventoryConfig',
+    'retailer.apps.RetailerConfig',
     'product.apps.ProductConfig',
 
 ]
